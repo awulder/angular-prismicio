c555162c1438ebaee99a42cae882c5bb5df88242
awulder <info@wulder.nl>
2014-01-08 23:30:03 +0100

    Outline for the tests that needs to be written

--- test/prismicSpec.js
+++ test/prismicSpec.js
@@ -1,10 +1,55 @@
 'use strict';
 
-describe('prismi', function() {
+describe('Prismic', function() {
+  var Prismic, $httpBackend;
 
-  beforeEach(module('awulder.prismic-io'));
+  beforeEach(angular.mock.module('prismic.io'));
 
-  it('should pass (dummy test', function() {
-    expect(true).toBeTruthy();
-  })
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    Prismic = $injector.get('Prismic');
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('configuration', function() {
+    it('should set a default link resolver', function() {
+      expect(true).toBeTruthy();
+    })
+  });
+
+  describe('method descriptions', function() {
+    describe('all()', function() {
+      it('should return several documents', function() {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('query()', function() {
+      it('should return several documents', function() {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('document(id)', function() {
+      it('should return the document by id', function() {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('documents(ids)', function() {
+      it('should return the document by id', function() {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('bookmark(bookmkark)', function() {
+      it('should return the bookmarked document', function() {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
 });
