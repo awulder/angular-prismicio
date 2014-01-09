30e1a9f5bb7f6cd7b7f71e021e3badb95917f758
awulder <info@wulder.nl>
2014-01-10 00:08:45 +0100

    Added missing dependency

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,9 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/prismic.io/dist/prismic.io-1.0.5.min.js',
       'bower_components/underscore/underscore.js',
-      'src/prismic.js',
+      'src/angularPrismicIo.js',
       'test/*.js'
     ],
     reporters: ['progress'],
