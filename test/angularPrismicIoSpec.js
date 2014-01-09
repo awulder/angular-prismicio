14c96be3b3c35e400162db37a0af822fbb87048c
awulder <info@wulder.nl>
2014-01-10 00:07:51 +0100

    Renamed file and added tests

---
+++ test/angularPrismicIoSpec.js
@@ -0,0 +1,143 @@
+'use strict';
+
+describe('Prismic', function() {
+  var $httpBackend, Prismic, result;
+
+  var apiEndpoint = 'https://lesbonneschoses.prismic.io/api';
+
+  var contextResponse = function() {
+    return {
+      "refs": [
+        {
+          "ref": "UkL0hcuvzYUANCrm",
+          "label": "Master",
+          "isMasterRef": true
+        }
+      ],
+      "bookmarks": {
+        "products": "UkL0gMuvzYUANCpt"
+      },
+      "types": {
+        "product": "Product"
+      },
+      "tags": [],
+      "forms": {
+        "everything": {
+          "method": "GET",
+          "enctype": "application/x-www-form-urlencoded",
+          "action": "https://lesbonneschoses.prismic.io/api/documents/search",
+          "fields": {
+            "ref": {
+              "type": "String",
+              "multiple": false
+            },
+            "q": {
+              "type": "String",
+              "multiple": true
+            },
+            "page": {
+              "type": "Integer",
+              "multiple": false,
+              "default": "1"
+            },
+            "pageSize": {
+              "type": "Integer",
+              "multiple": false,
+              "default": "20"
+            },
+            "orderings": {
+              "type": "String",
+              "multiple": false
+            }
+          }
+        }
+      },
+      "oauth_initiate": "https://lesbonneschoses.prismic.io/auth",
+      "oauth_token": "https://lesbonneschoses.prismic.io/auth/token"
+    };
+  };
+
+  var searchResponse = function() {
+    return {
+      results: [
+        {
+          id: 'UkL0gMuvzYUANCpf',
+          type: 'product',
+          href: '',
+          tags: [],
+          slug: '-',
+          fragments: {},
+          data: {}
+        }
+      ]
+    };
+  };
+
+  beforeEach(module('prismic.io'));
+
+  beforeEach(inject(function(_$httpBackend_, _Prismic_) {
+    $httpBackend = _$httpBackend_;
+    Prismic = _Prismic_;
+    Prismic.setApiEndpoint(apiEndpoint);
+  }));
+
+  beforeEach(function() {
+    $httpBackend.expectGET(apiEndpoint).respond(contextResponse());
+  });
+
+  afterEach(function() {
+    $httpBackend.flush()
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('method descriptions', function() {
+    it('should issue GET query', function() {
+      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm&q=%5B%3Ad%20%3D%20at(document.type%2C%20%22product%22)%5D')
+        .respond(searchResponse());
+
+      Prismic.query('[:d = at(document.type, "product")]').then(function(queryResult) {
+        result = queryResult;
+      });
+    });
+
+    it('should issue GET all', function() {
+      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm')
+        .respond(searchResponse());
+
+      Prismic.all().then(function(queryResult) {
+        result = queryResult;
+      });
+    });
+
+    it('should issue GET document', function() {
+      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm&q=%5B%5B%3Ad%20%3D%20at(document.id%2C%20%221%22)%5D%5D')
+        .respond(searchResponse());
+
+      Prismic.document(1).then(function(queryResult) {
+        result = queryResult;
+      });
+    });
+
+    it('should issue GET documents', function() {
+      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm&q=%5B%5B%3Ad%20%3D%20at(document.id%2C%20%221%22)%5D%5D')
+        .respond(searchResponse());
+
+      Prismic.document(1).then(function(queryResult) {
+        result = queryResult;
+      });
+    });
+
+//    it('should issue GET bookmark', function() {
+//      $httpBackend.expectGET(apiEndpoint).respond(contextResponse());
+//      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm&q=%5B%5B%3Ad%20%3D%20at(document.id%2C%20%221%22)%5D%5D')
+//        .respond();
+//      $httpBackend.expectGET(apiEndpoint + '/documents/search?page=1&pageSize=20&ref=UkL0hcuvzYUANCrm&q=%5B%5B%3Ad%20%3D%20at(document.id%2C%20%221%22)%5D%5D')
+//        .respond(searchResponse());
+//
+//      Prismic.bookmark('products').then(function(queryResult) {
+//        result = queryResult;
+//      });
+//    });
+  });
+});
